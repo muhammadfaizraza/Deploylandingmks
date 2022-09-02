8065e13767467bd57856d473d5b4f278ac63ecae
hammadaslam10 <muhammadaslambhai@gmail.com>
2022-09-02 19:18:38 +0500

    first

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HttpApi from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import 'bootstrap/dist/js/bootstrap.js'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'ar'],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  })
+
+    // react: { useSuspense: false },
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h1 data-text="Loading...">Loading...</h1>
+  </div>
+)
+
+ReactDOM.render(
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById('root')
+)
